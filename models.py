d36fcc21f0b8e49a6e4030864b1b69219c8b78ee
Eric Garnick <eric.b.garnick@gmail.com>
2021-12-14 21:19:59 -0500

    Added demo model

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Resource(models.Model):
     resource_id = models.AutoField(primary_key=True)
     resouce_link = models.URLField()
 
+
+class DemoModel(models.Model):
+    demo_field = models.CharField(max_length=50)
 
